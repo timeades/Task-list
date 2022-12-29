87e5fba849dbaea12e6df0138d8cc56122805635
timeades <tim.eades@mac.com>
2022-12-29 16:27:37 +0000

    first test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,20 +2,25 @@
 const { test, expect } = require('@playwright/test');
 
 test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://127.0.0.1:5500/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Todo List 2022 with LocalStorage/);
 
-  // create a locator
-  const getStarted = page.getByRole('link', { name: 'Get started' });
+  // Expect the page to have a create, category and list section
+  await expect(page).toContainEqual('CREATE A TODO');
+  // await expect(page).toContain('Pick a category');
+  // await expect(page).toContain('TODO LIST');
 
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+  // // create a locator
+  // const getStarted = page.getByRole('link', { name: 'Get started' });
 
-  // Click the get started link.
-  await getStarted.click();
+  // // Expect an attribute "to be strictly equal" to the value.
+  // await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+
+  // // Click the get started link.
+  // await getStarted.click();
   
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  // // Expects the URL to contain intro.
+  // await expect(page).toHaveURL(/.*intro/);
 });
